34b6b72cb425d24caadf028a52907a620b73b321
aliagmr <www.aliaghamiry@gmail.com>
2022-06-30 13:11:17 +0430

    add categories endpoint

--- index.js
+++ index.js
@@ -61,5 +61,31 @@ app.post('/api/register', (req, res) => {
 
 });
 
+// Get Categories & Subcategories Endpoint
+app.get('/api/categories', (req, res) => {
+    const query = `SELECT * FROM category`;
+    db.any(query)
+        .then(categories => {
+            let categories_list = [];
+            categories.forEach(category => {
+                if (category.parent_id === null) {
+                    categories_list.push(category);
+                } else {
+                    let parent_category = categories_list.find(cat => cat.id === category.parent_id);
+                    if (parent_category) {
+                        if (!parent_category.subcategories) {
+                            parent_category.subcategories = [];
+                        }
+                        parent_category.subcategories.push(category);
+                    }
+                }
+            })
+            res.json({categories: categories_list, success: true, code: 200});
+        }).catch(err => {
+            res.status(200).json({message: 'Failed to get categories', success: false, code: 200});
+        }
+    );
+});
+
 app.listen(8080);
 console.log(`[\x1b[90m${new Date().toTimeString().split(' ')[0]}\x1b[0m] Server is listening on port 8080`);
